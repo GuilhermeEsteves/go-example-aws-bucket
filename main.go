0c0460c833fcdd824bb20f5d97c7d5d7bf6c3f0d
Guilherme Esteves <guilherme.esteves@luizalabs.com>
2018-04-11 17:04:55 -0300

    implements copy and delete

--- main.go
+++ main.go
@@ -11,8 +11,9 @@ import (
 )
 
 var (
-	region = "us-east-1"
-	bucket = "dynamo-price-research-hml"
+	region      = "us-east-1"
+	ourBucket   = "our-bucket"
+	theirBucket = "their-bucket"
 )
 
 func main() {
@@ -22,7 +23,7 @@ func main() {
 
 	svc := s3.New(sess)
 
-	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket)})
+	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(theirBucket)})
 
 	if err != nil {
 		log.Fatal("Error in list", err)
@@ -33,16 +34,18 @@ func main() {
 		fmt.Println("Last modified:", *item.LastModified)
 		fmt.Println("Size:         ", *item.Size)
 		fmt.Println("Storage class:", *item.StorageClass)
-		fmt.Println("key", *item.Key)
-		fmt.Println("*******Content********")
+		sendItemAnotherBucket(svc, item)
+		deleteFileBucket(svc, item)
+		fmt.Println("\n*******Content********")
 		getObject(svc, item)
 		fmt.Println("*******End Content********")
+		break
 	}
 }
 
 func getObject(svc *s3.S3, item *s3.Object) {
 	out, err := svc.GetObject(&s3.GetObjectInput{
-		Bucket: aws.String(bucket),
+		Bucket: aws.String(ourBucket),
 		Key:    item.Key,
 	})
 
@@ -53,4 +56,37 @@ func getObject(svc *s3.S3, item *s3.Object) {
 
 	content, _ := ioutil.ReadAll(out.Body)
 	fmt.Println(string(content))
+}
+
+func sendItemAnotherBucket(svc *s3.S3, item *s3.Object) {
+	source := theirBucket + "/" + *item.Key
+	_, err := svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(ourBucket), CopySource: aws.String(source), Key: aws.String(*item.Key)})
+	if err != nil {
+		fmt.Println("Unable to copy item from bucket", err)
+	}
+
+	err = svc.WaitUntilObjectExists(&s3.HeadObjectInput{Bucket: aws.String(ourBucket), Key: aws.String(*item.Key)})
+	if err != nil {
+		fmt.Println("Error occurred while waiting for item  to be copied to bucket")
+	}
+
+	fmt.Println("File copied with success!")
+}
+
+func deleteFileBucket(svc *s3.S3, item *s3.Object) {
+	_, err := svc.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(theirBucket), Key: aws.String(*item.Key)})
+	if err != nil {
+		fmt.Println("Unable to delete object from bucket")
+	}
+
+	err = svc.WaitUntilObjectNotExists(&s3.HeadObjectInput{
+		Bucket: aws.String(theirBucket),
+		Key:    aws.String(*item.Key),
+	})
+
+	if err != nil {
+		fmt.Printf("Error occurred while waiting for object %q to be deleted\n", *item.Key)
+	}
+
+	fmt.Printf("File deleted with sucess!")
 }
